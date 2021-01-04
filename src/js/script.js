25509908b366601668957b6620de8844186cd8d8
Mad Zephyr <v21400@gmail.com>
2021-01-04 18:09:46 +0200

    Adding Vue

--- src/js/script.js
+++ src/js/script.js
@@ -3,15 +3,34 @@ const Credit = {
         return {
             label: 'Фамилия Имя Отчество',
             placeHolderString: 'Ваше имя',
-            nameValue: ''
+            nameValue: '',
+            rangeValue: 5000,
+            timeValue: '6',
+            show: false
         };
     },
     methods: {
         nameHandler(event){
-            this.nameValue = event.target.value
-            console.log(this.nameValue);
+            this.nameValue = event.target.value 
+        },
+        getRangeValue(event){
+            this.rangeValue = event.target.value 
+        },
+        getTimeValue(event){
+            console.log(event.target.value)
+            this.timeValue = event.target.value
+        },
+        takeUser(event){
+            if(event.type === 'click'){
+                if (this.nameValue !='' && this.timeValue !=''){
+                    this.show = true
+                }
+
+            }
+
+            
         }
     }
 };
 
-Vue.createApp(Credit).mount('.main');
+Vue.createApp(Credit).mount('body');
