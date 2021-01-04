8d9a1efa9d49630e5dabb6cbe48f51e8107cb653
Mad Zephyr <v21400@gmail.com>
2021-01-05 00:39:08 +0200

    coding with VUE

--- src/js/script.js
+++ src/js/script.js
@@ -6,29 +6,48 @@ const Credit = {
             nameValue: '',
             rangeValue: 5000,
             timeValue: '6',
-            show: false
+            show: false,
+            rb: false,
+            redText: false,
+            creditGiver: 0
         };
     },
     methods: {
-        nameHandler(event){
-            this.nameValue = event.target.value 
+        getNameValue(event){
+            this.nameValue = event.target.value
         },
-        getRangeValue(event){
-            this.rangeValue = event.target.value 
+        getRangeValue(event){ 
+            this.rangeValue = event.target.value; 
         },
-        getTimeValue(event){
-            console.log(event.target.value)
-            this.timeValue = event.target.value
+        getTimeValue(event){ 
+            this.timeValue = event.target.value;
         },
         takeUser(event){
             if(event.type === 'click'){
                 if (this.nameValue !='' && this.timeValue !=''){
-                    this.show = true
-                }
-
+                    this.show = true;
+                } 
             }
-
-            
+            if (this.nameValue === ''){
+                console.log('RB');
+                this.rb = true;
+                this.label = 'Введите ФИО';
+                this.redText = true;
+            } else {
+                this.rb = false;
+                this.label = 'Фамилия Имя Отчество';
+                this.redText = false;
+            }
+        }
+    },
+    computed: {
+        getNameValueComputed(){
+            return this.nameValue 
+        }
+    },
+    watch: {
+        nameValue(value){
+            console.log('changed', value);
         }
     }
 };
